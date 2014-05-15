0494e369030e7c43244cfb20526e282d3e609541
Dominik <schdomin@gmail.com>
2014-05-15 18:37:40 +0200

    updated

--- runzep_linux.sh
+++ runzep_linux.sh
@@ -3,15 +3,15 @@
 #ds check if java dir is not set
 if [ "$JAVA_HOME" = "" ]; then
 
- #run zep from PATH variable
- echo "Running ZEP from PATH variable"
- java -cp bin/.:thirdparty/* main.CMain
-
+    #run zep from PATH variable
+    echo "Running ZEP from PATH variable"
+    java -cp bin/.:thirdparty/* main.CMain
+    
 else
 
- #run zep from JAVA_HOME variable
- echo "Running ZEP from JAVA_HOME variable"
- $JAVA_HOME/bin/java -cp bin/.:thirdparty/* main.CMain 
-
+    #run zep from JAVA_HOME variable
+    echo "Running ZEP from JAVA_HOME variable"
+    $JAVA_HOME/bin/java -cp bin/.:thirdparty/* main.CMain 
+    
 fi
 
--- runzep_windows.bat
+++ runzep_windows.bat
@@ -1,15 +1,49 @@
 :ds disable echo
 @ECHO OFF
 
+:ds set size
+mode con:cols=110 lines=30
+
 :ds check if java dir is not set
-IF "%JAVA_HOME%"=="" (
+if "%JAVA_HOME%"=="" (
 
-ECHO JAVA_HOME not set - calling java from PATH variable
-java -cp bin\.;thirdparty\*; main.CMain
+:ds look for java.exe
+for %%X in (java.exe) do (set FOUND=%%~$PATH:X)
 
-) ELSE (
+:ds if we found the executable
+if defined FOUND (
 
-ECHO JAVA_HOME set - calling java from JAVA_HOME
+:ds run from PATH
+echo "LAUNCH: JAVA_HOME not set - calling java from PATH variable"
+java -cp bin\.;thirdparty\*; main.CMain 
+pause
+
+) else (
+
+:ds if programfiles(x86) is not set
+if "%ProgramFiles(x86)%"=="" (
+
+:ds run from default directory
+echo "LAUNCH: no useful environment variable set - running from standart path"
+"%ProgramFiles%\Java\jre7\bin\java.exe" -cp bin\.;thirdparty\*; main.CMain
+pause
+
+) else (
+
+:ds run from default directory
+echo "LAUNCH: ProgramFiles(x86) set - calling java.exe"
+"%ProgramFiles(x86)%\Java\jre7\bin\java.exe" -cp bin\.;thirdparty\*; main.CMain
+pause
+
+)
+
+)
+
+) else (
+
+:ds run from JAVA_HOME
+echo "LAUNCH: JAVA_HOME set - calling java from JAVA_HOME"
 %JAVA_HOME%\bin\java -cp bin\.;thirdparty\*; main.CMain
+pause
 
 )
