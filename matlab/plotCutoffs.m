4e1d6a4e097f691b24d581387e9fc78b40e74227
Dominik <schdomin@gmail.com>
2014-06-10 22:52:46 +0200

    updated

--- matlab/plotCutoffs.m
+++ matlab/plotCutoffs.m
@@ -8,12 +8,38 @@ vecCutoffs  = matCutoffs( :, 1 );
 vecTypes    = matCutoffs( :, 2 );
 vecPoints   = matCutoffs( :, 3 );
 
+%ds compute relative datapoints/tag types
+vecRelative = vecPoints./( vecTypes+vecCutoffs );
+ 
+%ds plot
+figure( 1 );
+hPlot = plot( vecCutoffs, vecRelative, 'blue' );
+axis( [0, vecCutoffs( end ), 0, 90 ] );
+title( 'Ratio f(x): $\frac{DataPoints(x)}{TagTypes(x)+x}$', 'interpreter', 'latex' );
+xlabel( 'x: Cutoff frequency', 'interpreter', 'latex' );
+ylabel( 'f(x): Ratio', 'interpreter', 'latex' );
+grid on;
+
+%ds snippet: get max and min values of the plot
+xValue = get( hPlot,'XData' );
+yValue = get( hPlot,'YData' );
+imax = find( max( yValue ) == yValue );
+
+%ds best cutoff
+iBestCutoff = xValue( imax );
+
+%ds label the max. and min. values  on the plot
+text( xValue( imax ),yValue( imax ),[ ' $f_{max}=',num2str( yValue( imax ) ), '(x=',num2str( iBestCutoff ),')$' ],...
+     'interpreter', 'latex', 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'left' );
+
+saveas( 1, 'cutoff_choice.eps', 'epsc' );
+
 %ds get max values
 iMaximumTypes  = max( vecTypes );
 iMaximumPoints = max( vecPoints );
 
 %ds mark cutoff choice
-iIndexCutoff = find( vecCutoffs==104 );
+iIndexCutoff = find( vecCutoffs==iBestCutoff );
 iCoordinateX = vecCutoffs( iIndexCutoff );
 iCoordinateY = vecPoints( iIndexCutoff );
 iTypes       = vecTypes( iIndexCutoff );
@@ -23,38 +49,17 @@ iTypes       = vecTypes( iIndexCutoff );
 %vecNormalizedPoints = vecPoints./iMaximumPoints;
 
 %ds plot
-figure( 1 );
+figure( 2 );
 plot( vecCutoffs, vecTypes, 'blue', vecCutoffs, vecPoints, 'red' );
-line( [ iCoordinateX, iCoordinateX ], [0, iCoordinateY ], 'Color', 'black' );
+line( [ iCoordinateX, iCoordinateX ], [0, iCoordinateY ], 'Color', 'green' );
 axis( [0, vecCutoffs( end ), 0, iMaximumPoints ] );
 set(gca,'YTickLabel',num2str(get(gca,'YTick').'));
 grid on;
 title( 'Cutoff Relation', 'interpreter', 'latex' );
 xlabel( 'Cutoff frequency', 'interpreter', 'latex' );
 ylabel( 'Amount', 'interpreter', 'latex' );
-text( iCoordinateX, iCoordinateY, [ ' cutoff 104: points=', num2str( iCoordinateY ), ', types=', num2str( iTypes ) ], 'interpreter', 'latex', 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'left' );
+text( iCoordinateX, iCoordinateY, [ ' cutoff ', num2str( iBestCutoff ),': points=', num2str( iCoordinateY ), ', types=', num2str( iTypes ) ], 'interpreter', 'latex', 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'left' );
 hLegend = legend( [ 'Tag types (total: ', num2str( iMaximumTypes ), ')\hspace{0.5cm}' ], [ 'Datapoints (total: ', num2str( iMaximumPoints ),  ')\hspace{0.5cm}' ], 'Current cutoff choice ($104$)' );
 set( hLegend, 'interpreter', 'Latex' );
-saveas( 1, 'cutoffs.pdf' );
 
-%ds compute relative datapoints/tag types
-vecRelative = vecPoints./( vecTypes+vecCutoffs );
- 
-%ds plot
-figure( 2 );
-hPlot = plot( vecCutoffs, vecRelative, 'blue' );
-axis( [0, vecCutoffs( end ), 0, 90 ] );
-title( 'Ratio f(x): $\frac{DataPoints(x)}{TagTypes(x)+x}$', 'interpreter', 'latex' );
-xlabel( 'x: Cutoff frequency', 'interpreter', 'latex' );
-ylabel( 'f(x): Ratio', 'interpreter', 'latex' );
-
-%ds snippet: get max and min values of the plot
-xValue = get( hPlot,'XData' );
-yValue = get( hPlot,'YData' );
-imax = find( max( yValue ) == yValue );
-
-%ds label the max. and min. values  on the plot
-text( xValue( imax ),yValue( imax ),[ ' $f_{max}=',num2str( yValue( imax ) ), '(x=',num2str( xValue( imax ) ),')$' ],...
-     'interpreter', 'latex', 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'left' );
-
-saveas( 2, 'cutoffs_relative.pdf' );
+saveas( 2, 'cutoff_curve.eps', 'epsc' );
